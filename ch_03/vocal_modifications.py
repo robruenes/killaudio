5f4cad3a84986ce828335bf3f1c63f3aa0080b6d
Rob Ruenes <rob@ruenes.dev>
2022-01-05 12:08:47 -0600

    Add reversal edit.

---
+++ ch_03/vocal_modifications.py
@@ -0,0 +1,29 @@
+from scipy.io import wavfile
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def create_spliced_edit(sample_rate, data):
+    first_slice = data[:47609]
+    second_slice = data[123252:]
+    final_edit = np.concatenate([first_slice, second_slice])
+    wavfile.write("Vocal_spliced_edit.wav", sample_rate, final_edit)
+
+
+def create_reversed_edit(sample_rate, data):
+    # The below is equivalent to using
+    # the flip function in numpy:
+    #   reversed = np.flip(data)
+    # We use the slice syntax here instead to build off the examples
+    # from ch_03.ipynb.
+    reversed = data[-1:0:-1]
+    wavfile.write("Vocal_reversed_edit.wav", sample_rate, reversed)
+
+
+if __name__ == "__main__":
+    # Vocal.wav is another single channel audio file.
+    # The sample rate is 44100, and there are 215444
+    # samples.
+    sample_rate, data = wavfile.read("Vocal.wav")
+    create_spliced_edit(sample_rate, data)
+    create_reversed_edit(sample_rate, data)
